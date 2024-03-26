afa5cb06890b445be41b0f5b583efbeb5a05a01f
insistwind <578009720@qq.com>
2024-03-26 17:06:33 +0800

    dying lack of encoding and background control system

---
+++ framework/src/main/java/com/ning/domain/dto/ResumeCommitDto.java
@@ -0,0 +1,7 @@
+/**
+* @author: qjn
+* @create: 2024/03/25 21:47
+**/
+public class ResumeCommitDto {
+
+}
---
+++ framework/src/main/java/com/ning/domain/vo/WorkPageVo.java
@@ -0,0 +1,7 @@
+/**
+* @author: qjn
+* @create: 2024/03/24 10:35
+**/
+public class WorkPageVo {
+
+}
